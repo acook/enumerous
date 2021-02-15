47692e7d2e62f01cbc0711f408876a404b1bd037
Anthony Cook <github@anthonymcook.com>
2021-02-14 19:57:26 -0600

    Fallback to a given value if no matches

--- src/enumerous/find_and_map.cr
+++ src/enumerous/find_and_map.cr
@@ -1,10 +1,11 @@
 module Enumerous::FindAndMap
   module FindAndMapEnumerable
-    def find_and_map
+    def find_and_map(fallback = nil)
       self.each do |*element|
         result = yield *element
         return result if result
       end
+      fallback
     end
   end
 
