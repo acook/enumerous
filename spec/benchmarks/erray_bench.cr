1aa0949dbb470bd077f3aae4927f580b1d24bd1e
Anthony Cook <github@anthonymcook.com>
2021-02-15 02:41:24 -0600

    Sanity check the operation

--- spec/benchmarks/erray_bench.cr
+++ spec/benchmarks/erray_bench.cr
@@ -1,6 +1,20 @@
 require "./bench_helper"
 require "../../src/enumerous/erray"
 
+a = Array(Int32).new
+a << 1
+a << 2
+a << 3
+
+p a, instance_sizeof(typeof(a))
+
+e = Erray(Int32).new
+e << 1
+e << 2
+e << 3
+
+p e, instance_sizeof(typeof(e))
+
 Benchmark.ips do |x|
   x.report("Array creation") do
     a = Array(Int32).new
@@ -10,9 +24,9 @@ Benchmark.ips do |x|
   end
 
   x.report("Erray creation") do
-    a = Erray(Int32).new
-    a << 1
-    a << 2
-    a << 3
+    e = Erray(Int32).new
+    e << 1
+    e << 2
+    e << 3
   end
 end
